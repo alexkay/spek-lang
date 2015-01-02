37c1c88c6437e916ef3d99604d142cede645386a
Alexander Kojevnikov <alexander@kojevnikov.com>
2015-01-01 20:21:21 -0800

    Add Go implementation

--- Makefile
+++ Makefile
@@ -2,15 +2,18 @@ CC = cc
 CFLAGS = -O2 -std=c99 -static $(shell pkg-config --cflags fftw3f)
 LDFLAGS = $(shell pkg-config --static --libs fftw3f)
 
-all: samples.bin spek-c
+all: samples.bin spek-c spek-go
 
 clean:
-	rm -f spek-c
+	rm -f samples.bin spek-c
 
 samples.bin:
 	dd if=/dev/urandom of=samples.bin bs=256 count=2480625
 
 spek-c: spek-lang.c
 	$(CC) $(CFLAGS) -o spek-c spek-lang.c $(LDFLAGS)
+
+spek-go: spek-lang.go
+	go fmt spek-lang.go && go build --ldflags '-extldflags "-static"' -o spek-go spek-lang.go
 
 .PHONY: clean
---
+++ spek-lang.go
@@ -0,0 +1,60 @@
+package main
+
+/*
+#cgo pkg-config: fftw3f
+#include <fftw3.h>
+*/
+import "C"
+
+import (
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"unsafe"
+)
+
+func main() {
+	const n = 2048
+	const numBytes = n * 4
+
+	input := C.fftwf_alloc_real(n)
+	floats := (*[1 << 30]float32)(unsafe.Pointer(input))[:n:n]
+	ints := (*[1 << 30]int32)(unsafe.Pointer(input))[:n:n]
+	bytes := (*[1 << 30]byte)(unsafe.Pointer(input))[:numBytes:numBytes]
+
+	output := C.fftwf_alloc_complex(n/2 + 1)
+	complex := (*[1 << 30]float32)(unsafe.Pointer(output))[:(n/2+1)*2 : (n/2+1)*2]
+	p := C.fftwf_plan_dft_r2c_1d(n, input, output, C.FFTW_ESTIMATE)
+	const n2 = n * n
+
+	result := 0
+
+	for {
+		_, err := io.ReadFull(os.Stdin, bytes)
+		if err != nil {
+			break
+		}
+		for i := 0; i < n; i++ {
+			floats[i] = float32(ints[i]) / math.MaxUint32
+		}
+
+		C.fftwf_execute_dft_r2c(p, input, output)
+
+		for i := 0; i <= n/2; i++ {
+			re := complex[i*2]
+			im := complex[i*2+1]
+			magnitude := float32(10.0 * math.Log10(float64((re*re+im*im)/n2)))
+
+			if magnitude > -48.0 {
+				result++
+			}
+		}
+	}
+
+	fmt.Printf("%d\n", result)
+
+	C.fftwf_destroy_plan(p)
+	C.fftwf_free(unsafe.Pointer(output))
+	C.fftwf_free(unsafe.Pointer(input))
+}
